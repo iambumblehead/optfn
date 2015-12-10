5a9f98bafa858684e068cc7627d86f9124fb5114
chris <chris@bumblehead.com>
2015-12-09 22:32:44 -0800

    added definition of main in package.json

--- optfn.js
+++ optfn.js
@@ -1,10 +1,9 @@
 // Filename: optfn.js
 // Timestamp: 2014.03.20-13:48:33 (last modified)  
 // Author(s): Bumblehead (www.bumblehead.com)
-// Requires:
 
 
-var optfn = ((typeof module === 'object') ? module : {}).exports = (function (b) {
+var optfn = module.exports = (function (b) {
 
   return function (fn) {
     return typeof fn === "function" ? fn : b;
